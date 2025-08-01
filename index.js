4fae76180dd1f6991d4399a3345b991985cc2f94
Kapil0204 <kapilkulkarni1991@gmail.com>
2025-08-01 09:42:33 +0000

    Final fix: use AMADEUS_API_KEY and AMADEUS_API_SECRET

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ app.post('/search', async (req, res) => {
       'https://test.api.amadeus.com/v1/security/oauth2/token',
       new URLSearchParams({
         grant_type: 'client_credentials',
-        client_id: process.env.AMADEUS_CLIENT_ID,
-client_secret: process.env.AMADEUS_CLIENT_SECRET,
+        client_id: process.env.AMADEUS_API_KEY,
+        client_secret: process.env.AMADEUS_API_SECRET
       }),
       {
         headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
